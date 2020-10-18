60d3d9c1ac9274ab8bbfe7d457cd1dffbabcfc04
Karan Budhwani <karanbd86@gmail.com>
2020-10-18 23:05:11 +0530

    menu added, options remaining

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import './menu.scss';
 const Menu = (props: any) => {
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+    const [hamClasses, setHamClasses] = useState<string[]>(['hamburger-menu']);
+    const [menuClasses, setMenuClasses] = useState<string[]>(['menu-items']);
+
+    // let hamClasses = ['hamburger-menu'];
+
+    const onMenuBtnClick = () => {
+        if(!isMenuOpen) {
+            setHamClasses(['hamburger-menu', 'open'])
+            setMenuClasses(['menu-items', 'menu-open'])
+            console.log(hamClasses, hamClasses.join(' '));
+            setIsMenuOpen(true);
+        } else {
+            setHamClasses(['hamburger-menu']);
+            setMenuClasses(['menu-items'])
+            console.log(hamClasses, hamClasses.join(' '));
+            setIsMenuOpen(false);
+        }
+    }
+
+    const MenuItems = () => (
+        <div className={menuClasses.join(' ')}>
+
+        </div>
+    )
     return (
         <div className="menu-wrap">
             <div className="menu-list">
@@ -34,6 +59,11 @@ const Menu = (props: any) => {
                            color: props.color
                        }}>Services</a>
                 </span>
+            </div>
+
+            <div className={hamClasses.join(' ')} onClick={onMenuBtnClick}>
+                <div className="menu-btn"></div>
+                <MenuItems />
             </div>
         </div>
     );
