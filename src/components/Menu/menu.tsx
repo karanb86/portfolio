201a7b2a2d12a45d600b6e2fa492a3daee2c00db
Karan Budhwani <karanbd86@gmail.com>
2020-10-18 18:26:16 +0530

    header and menu responsive

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -3,36 +3,38 @@ import './menu.scss';
 const Menu = (props: any) => {
     return (
         <div className="menu-wrap">
-            <span>
-                <a href=""
-                   style={{
-                       color: props.color
-                   }}>About Me</a>
-            </span>
-            <span>
-                <a href=""
-                   style={{
-                       color: props.color
-                   }}>My Resume</a>
-            </span>
-            <span>
-                <a href=""
-                   style={{
-                       color: props.color
-                   }}>Education</a>
-            </span>
-            <span>
-                <a href=""
-                   style={{
-                       color: props.color
-                   }}>Skills</a>
-            </span>
-            <span>
-                <a href=""
-                   style={{
-                       color: props.color
-                   }}>Services</a>
-            </span>
+            <div className="menu-list">
+                <span>
+                    <a href=""
+                       style={{
+                           color: props.color
+                       }}>About Me</a>
+                </span>
+                <span>
+                    <a href=""
+                       style={{
+                           color: props.color
+                       }}>My Resume</a>
+                </span>
+                <span>
+                    <a href=""
+                       style={{
+                           color: props.color
+                       }}>Education</a>
+                </span>
+                <span>
+                    <a href=""
+                       style={{
+                           color: props.color
+                       }}>Skills</a>
+                </span>
+                <span>
+                    <a href=""
+                       style={{
+                           color: props.color
+                       }}>Services</a>
+                </span>
+            </div>
         </div>
     );
 }
