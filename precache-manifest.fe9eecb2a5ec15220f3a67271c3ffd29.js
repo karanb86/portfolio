644893871d6ccfb35150038a7bc0c89883b06a21
Karan Budhwani <karanbd86@gmail.com>
2021-05-22 20:59:15 +0530

    Updates

---
+++ precache-manifest.fe9eecb2a5ec15220f3a67271c3ffd29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dea269b356890597b509c7a2c4d19a6",
+    "url": "/karanb86.github.io/index.html"
+  },
+  {
+    "revision": "2d3264c61900a59513f1",
+    "url": "/karanb86.github.io/static/css/main.0efded16.chunk.css"
+  },
+  {
+    "revision": "2197ca2611f00a6b41a6",
+    "url": "/karanb86.github.io/static/js/2.f848e824.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/karanb86.github.io/static/js/2.f848e824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3264c61900a59513f1",
+    "url": "/karanb86.github.io/static/js/main.ce5d4dfa.chunk.js"
+  },
+  {
+    "revision": "2d8fc0aaa4291b11955f",
+    "url": "/karanb86.github.io/static/js/runtime-main.1d606f0f.js"
+  },
+  {
+    "revision": "ee76b9d20271dcb3f85c3148cdb0c2ab",
+    "url": "/karanb86.github.io/static/media/logo.ee76b9d2.svg"
+  }
+]);
